c23d15435cb946ff25d2bf4007702c491040f1de
Kalakuh <fi.kalakuh@gmail.com>
2015-02-22 16:35:16 +0200

    Updates to ContextMenu

--- src/kalakuh/time4you/gui/GameContextMenu.hx
+++ src/kalakuh/time4you/gui/GameContextMenu.hx
@@ -2,6 +2,7 @@ package kalakuh.time4you.gui;
 	
 import flash.ui.ContextMenu;
 import flash.ui.ContextMenuItem;
+import openfl.events.ContextMenuEvent;
 
 	/**
 	 * ...
@@ -16,13 +17,17 @@ class GameContextMenu {
 		var menu : ContextMenu = new ContextMenu();
 		menu.hideBuiltInItems();
 		
-		var infinite : ContextMenuItem = new ContextMenuItem("Director: InfiniteDrop", true, false);
-		var kalakuh : ContextMenuItem = new ContextMenuItem("Programmer: Kalakuh", true, false);
-		var haudio : ContextMenuItem = new ContextMenuItem("Graphics Designer: Haudio", true, false);
-		var copyright : ContextMenuItem = new ContextMenuItem("Copyright - 2015", true, false);
+		var secret : ContextMenuItem = new ContextMenuItem("DO NOT CLICK ME!");
+		secret.addEventListener(ContextMenuEvent.MENU_ITEM_SELECT, onClick);
+		var solarfly : ContextMenuItem = new ContextMenuItem("Made by Solarfly Studios", true, false);
+		var copyright : ContextMenuItem = new ContextMenuItem("Copyright - 2015", false, false);
 		
-		menu.customItems = [infinite, kalakuh, haudio, copyright];
+		menu.customItems = [secret, solarfly, copyright];
 		
 		return menu;
+	}
+	
+	private static function onClick (e : ContextMenuEvent) : Void {
+		trace("Wow! Much secret! Very easter egg!");
 	}
 }
